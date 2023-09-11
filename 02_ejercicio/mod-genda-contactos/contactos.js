d089fa4308647062c9ef0fe83359e8d72c25a996
JuanRamonL <juan.lopez130896@gmail.com>
2023-09-11 15:41:20 -0300

    Actividad 2 - Módulos - Agenda de Contactos: Programa que permite gestionar una agenda de contactos permitiendo agregar contacontactos  validando que los datos ingresados sean validos

---
+++ 02_ejercicio/mod-genda-contactos/contactos.js
@@ -0,0 +1,38 @@
+
+import validaciones from './validaciones.js'
+
+var contactos = {
+    contacto1: {
+    nombre: "Juan López",
+    telefono: "555-123-4567",
+    email: "juan@example.com"
+    },
+  // Puedes agregar más contactos aquí
+};
+
+function agreagrContacto(nombre, telefono, email){
+    if (validaciones.validarTelefono(telefono) == true){
+    }else{
+        return console.log(`El numero ${telefono} no es valido`)
+    }   
+    if(validaciones.validarEmail(email) == true){
+    }else{
+        return console.log(`El email ${email} no es valido `)
+    }
+
+    //Obtenemos la cantidad de claves que tiene un objeto y le sumamos uno.
+    let nuevoId = `contacto${Object.keys(contactos).length + 1}`
+
+    contactos[nuevoId] = {
+        nombre: nombre,
+        telefono: telefono,
+        email: email
+    };
+
+    console.log("Contacto agregado con éxito.");
+}
+
+export default {
+    contactos,
+    agreagrContacto,
+}
--- 02_ejercicio/mod-genda-contactos/index.js
+++ 02_ejercicio/mod-genda-contactos/index.js
@@ -1,40 +1,10 @@
-import validaciones from './validaciones.js'
+import valiCont from './contactos.js'
 
-var contactos = {
-    contacto1: {
-    nombre: "Juan López",
-    telefono: "555-123-4567",
-    email: "juan@example.com"
-    },
-  // Puedes agregar más contactos aquí
-};
 
-function agreagrContacto(nombre, telefono, email){
-    if (validaciones.validarTelefono(telefono) == true){
-    }else{
-        return console.log(`El numero ${telefono} no es valido`)
-    }   
-    if(validaciones.validarEmail(email) == true){
-    }else{
-        return console.log(`El email ${email} no es valido `)
-    }
+console.log(valiCont.contactos)
 
-    //Obtenemos la cantidad de claves que tiene un objeto y le sumamos uno.
-    let nuevoId = `contacto${Object.keys(contactos).length + 1}`
+valiCont.agreagrContacto('Brian Lara', '123-456-7890', 'blara@email.com');
+valiCont.agreagrContacto('Juan Gonzalez', '123-456-7899','jgonzalez@email.com');
+valiCont.agreagrContacto('Error No Funciona', '000000','email-invalido')
 
-    contactos[nuevoId] = {
-        nombre: nombre,
-        telefono: telefono,
-        email: email
-    };
-
-    console.log("Contacto agregado con éxito.");
-}
-
-console.log(contactos)
-
-agreagrContacto('Brian Lara', '123-456-7890', 'blara@email.com');
-agreagrContacto('Juan Gonzalez', '123-456-7899','jgonzalez@email.com');
-agreagrContacto('Error No Funciona', '000000','email-invalido')
-
-console.log(contactos)
+console.log(valiCont.contactos)
